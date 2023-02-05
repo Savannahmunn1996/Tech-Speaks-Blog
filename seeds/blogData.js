c12d9a417e00f4e7f53fdfe7233cdc9b05546cae
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-02-05 11:52:36 -0800

    adding blog data

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -10,4 +10,8 @@ const blogData = [
     title: "Sequelize Basics",
     post: "Sequelize is a Node. js-based Object Relational Mapper that makes it easy to work with MySQL, MariaDB, SQLite, PostgreSQL databases, and more. An Object Relational Mapper performs functions like handling database records by representing the data as objects.",
   },
+  {
+    title: "Rest APIs",
+    post: "REST stands for representational state transfer.",
+  },
 ];
