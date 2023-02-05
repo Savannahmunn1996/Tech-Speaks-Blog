dff1960a1cf4cbed6b7d1cbd987e02bb247c5320
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-02-05 11:48:49 -0800

    blog data seeding

---
+++ seeds/blogData.js
@@ -0,0 +1,13 @@
+const { Blog } = require("../models");
+
+const blogData = [
+  {
+    title: "CSS Rules",
+    post: "A css selector targets HTML to apply styles to content.",
+  },
+
+  {
+    title: "Sequelize Basics",
+    post: "Sequelize is a Node. js-based Object Relational Mapper that makes it easy to work with MySQL, MariaDB, SQLite, PostgreSQL databases, and more. An Object Relational Mapper performs functions like handling database records by representing the data as objects.",
+  },
+];
